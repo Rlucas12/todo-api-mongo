b6da21ef6f6098f734fe37d912ca33079fd37749
REYNES Lucas <lucas.reynes@hawk-platform.io>
2018-05-23 23:28:00 +0200

    Update Project validation rules

--- src/routes/projectRouter.js
+++ src/routes/projectRouter.js
@@ -10,8 +10,13 @@ projectRouter.route('/')
     })
     .post((req, res) => {
         let project = new Project(req.body);
-        project.save();
-        res.status(201).send(project) 
+        project.save(err => {
+            if(err) {
+                res.status(500).send(err);
+            } else {
+                res.status(201).send(project)
+            }
+        });
     })
 
 // Middleware 
@@ -31,15 +36,16 @@ projectRouter.route('/:projectId')
         res.json(req.project)
     }) // end get projects/:projectId 
     .put((req,res) => {
-        if(!req.body.name || !req.body.color) {
-            res.status(403).send("Fields name, color can't be blank")
-        } else {
-            req.project.name = req.body.name;
-            req.project.color = req.body.color;
-            req.project.isArchived = req.body.isArchived;
-            req.project.save()
-            res.json(req.project)
-        }
+        req.project.name = req.body.name;
+        req.project.color = req.body.color;
+        req.project.isArchived = req.body.isArchived;
+        req.project.save(err => {
+            if(err) {
+                res.status(500).send(err);
+            } else {
+                res.json(req.project);
+            }
+        })
     })
     .patch((req,res)=>{
         if(req.body._id){
